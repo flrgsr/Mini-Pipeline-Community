a885c2d586347e0a3c8d55fe9c9a0c91531feb60
Florian Gesser <gesser.florian@googlemail.com>
2015-07-10 12:35:39 +0200

    community pipeline with 1D files as input

---
+++ community_pipeline_1D.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+import sys
+sys.path.append("..")
+import louvain
+
+def run_mini_pipeline():
+	datadir = 'data'
+	for root, dirs, filenames in os.walk(datadir):
+	    for file in filenames:
+	        print 'computing subject ' + str(filenames.index(file))
+	        file_handle = os.path.join(root, file)
+	        df = pd.read_csv(file_handle, sep='\t')
+	        np_ts_matrix=df.as_matrix()
+	        correlation_matrix = np.corrcoef(np_ts_matrix.T)
+	        threshold = 0.6
+	        correlation_matrix[correlation_matrix<threshold] = 0
+	        correlation_matrix[correlation_matrix != 0] = 1
+	        graph = nx.from_numpy_matrix(correlation_matrix)
+	        if filenames.index(file) == 0:
+	            dendrogram = louvain.generate_dendrogram(graph)
+	            print 'number of levels ' + str(len(dendrogram))
+	            print len(set(louvain.partition_at_level(dendrogram, 1)))
+	            print len(set(louvain.partition_at_level(dendrogram, len(dendrogram) - 1 )))
+	    print 'done'
+
+if __name__ == '__main__':
+	run_mini_pipeline()

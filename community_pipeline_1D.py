c42ee1a92fea7b6be1ef54b64462377e74612508
Florian Gesser <gesser.florian@googlemail.com>
2015-07-10 12:41:31 +0200

    add seperate function for community analysis

--- community_pipeline_1D.py
+++ community_pipeline_1D.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import networkx as nx
-
+import os
 import sys
 sys.path.append("..")
 import louvain
@@ -20,11 +20,15 @@ def run_mini_pipeline():
 	        correlation_matrix[correlation_matrix != 0] = 1
 	        graph = nx.from_numpy_matrix(correlation_matrix)
 	        if filenames.index(file) == 0:
-	            dendrogram = louvain.generate_dendrogram(graph)
-	            print 'number of levels ' + str(len(dendrogram))
-	            print len(set(louvain.partition_at_level(dendrogram, 1)))
-	            print len(set(louvain.partition_at_level(dendrogram, len(dendrogram) - 1 )))
+	            analyse_community(graph)
 	    print 'done'
+
+
+def analyse_community(graph):
+	dendrogram = louvain.generate_dendrogram(graph)
+	print 'number of levels ' + str(len(dendrogram))
+	print len(set(louvain.partition_at_level(dendrogram, 1)))
+	print len(set(louvain.partition_at_level(dendrogram, len(dendrogram) - 1 )))
 
 if __name__ == '__main__':
 	run_mini_pipeline()

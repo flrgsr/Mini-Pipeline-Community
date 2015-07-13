c177d63364c9666dc4f161bfcd963a68c873102f
Florian Gesser <gesser.florian@googlemail.com>
2015-07-13 18:24:21 +0200

    Added more verbose output

--- data/splitted/split.py
+++ data/splitted/split.py
@@ -3,7 +3,8 @@ import shutil
 
 files = glob.glob('*.1D')
 dest_list = ['group1', 'group2']
-count = 0
+all_count = 0
+bad_count = 0
 not_found = []
 
 with open('Yalegroups.txt', 'r') as grouplist:
@@ -16,13 +17,14 @@ with open('Yalegroups.txt', 'r') as grouplist:
 				dest_flag = 1
 				continue
 			try:
+				all_count += 1
 				dest_idx = files.index(line + '_rois_cc400.1D')
 				print 'moving ' + files[dest_idx] + ' to ' + dest_list[dest_flag]
 				shutil.move(files[dest_idx], dest_list[dest_flag])
 			except ValueError:
 				print line + ' not found'
 				not_found.append(line)
-				count += 1
-print '\n' + str(count) + ' files could not be moved as they don\'t exist: \n' + str(not_found)
+				bad_count += 1
+print '\n' + str(bad_count) + ' out of ' + str(all_count) + ' files could not be moved as they don\'t exist: \n' + str(not_found)
 			
 			
